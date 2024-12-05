a4f9c09fdeed758b0407e7dfe0136bc6f74809a3
Rakesh Vijay Kumar <ravi3136@colorado.edu>
2024-12-05 16:09:39 -0700

    Create SensorBatteryUsageApp.swift

---
+++ SensorBatteryUsageApp.swift
@@ -0,0 +1,15 @@
+// SensorBatteryUsage
+// Main SwiftUI App Entry Point
+
+import SwiftUI
+
+@main
+
+// Entry point for the app
+struct SensorBatteryUsageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
